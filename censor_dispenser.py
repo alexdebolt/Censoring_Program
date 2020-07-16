b100b1a8bbff93ef32c4577b5862b9612a0b6441
Alex DeBolt <58086131+alexdebolt@users.noreply.github.com>
2020-07-16 18:26:06 -0400

    figured out the steps needed to convert and replace text, did not need to iterate through email. Just need to do a replace call with censored text

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -6,4 +6,15 @@ email_four = open("email_four.txt", "r").read()
 
 
 def sensor_simple(email, text_to_censor):
-    pass
+    censored = ""
+    # this for loop censors the text regardless of if it is in the email, converts it to @ signs
+    for i in range(0, len(text_to_censor)):
+        if text_to_censor[i] == " ":
+            censored += " "
+        else:
+            censored += "@"
+    # if the text_to_censor is not in email, the email is unchanged
+    return email.replace(text_to_censor, censored)
+
+print(sensor_simple(email_one, "Till next month,"))
+
